52fbb3537dc8c2b8d19c9f23546cb088b45f0708
craigmj <craig@lateral.co.za>
2013-09-26 07:00:58 +0200

    Transaction support

--- Database.go
+++ Database.go
@@ -29,11 +29,34 @@ const (
 
 // ErrNotFound indicates that the key does not exist in the database.
 var ErrNotFound = errors.New("Key not found")
+// ErrTransactionInProgress returned when attempt to begin a transaction while there is already
+// a transaction in progress.
+var ErrTransactionInProgress = errors.New("Transaction already in progress")
 
 // Database is used for accessing a database.
 type Database struct {
 	unsafe.Pointer
 	env *Environment
+}
+
+// Begin starts a multi-statement transaction.
+//
+// During transaction, all updates are not written to the database files
+// until a sp_commit(3) is called. All updates made during transaction are
+// available through sp_get(3) or by using cursor.
+
+// No nested transactions are supported.
+func (db *Database) Begin() error {
+	switch C.sp_begin(db.Pointer) {
+	case 0:
+		return nil
+	case 1:
+		return ErrTransactionInProgress
+	case -1:
+		return db.Error()
+	}
+	// All cases should be handled by switch
+	return errors.New("Unexpected return from sp_begin, not in [-1,1]")
 }
 
 // Close closes the database and frees its associated memory. You must
@@ -45,6 +68,48 @@ func (db *Database) Close() error {
 	}
 	if nil != db.env {
 		return db.env.Close()
+	}
+	return nil
+}
+
+// Commit applies changes to a multi-statement
+// transaction. All modifications made during the transaction are written to 
+// the log file in a single batch.
+//
+// If commit failed, transaction modifications are discarded.
+func (db *Database) Commit() error {
+	e := C.sp_commit(db.Pointer)
+	if 0!=e {
+		return db.Error()
+	}
+	return nil
+}
+
+// Cursor returns a Cursor for iterating over rows in the database.
+//
+// If no key is provided, the Cursor will iterate over all rows.
+//
+// The order flag decides the direction of the iteration, and whether
+// the key is included or excluded.
+//
+// Iterate over values with Fetch or Next methods.
+func (db *Database) Cursor(order Order, key []byte) (*Cursor, error) {
+	cur := &Cursor{}
+	if nil == key {
+		cur.Pointer = C.sp_cursor(db.Pointer, C.sporder(order), unsafe.Pointer(nil), C.size_t(0))
+	} else {
+		cur.Pointer = C.sp_cursor(db.Pointer, C.sporder(order), unsafe.Pointer(&key[0]), C.size_t(len(key)))
+	}
+	if nil == cur.Pointer {
+		return nil, db.Error()
+	}
+	return cur, nil
+}
+
+// Delete deletes the key from the database.
+func (db *Database) Delete(key []byte) error {
+	if 0 != C.sp_delete(db.Pointer, unsafe.Pointer(&key[0]), C.size_t(len(key))) {
+		return db.Error()
 	}
 	return nil
 }
@@ -77,34 +142,6 @@ func (db *Database) Get(key []byte) ([]byte, error) {
 	return value, nil
 }
 
-// Cursor returns a Cursor for iterating over rows in the database.
-//
-// If no key is provided, the Cursor will iterate over all rows.
-//
-// The order flag decides the direction of the iteration, and whether
-// the key is included or excluded.
-//
-// Iterate over values with Fetch or Next methods.
-func (db *Database) Cursor(order Order, key []byte) (*Cursor, error) {
-	cur := &Cursor{}
-	if nil == key {
-		cur.Pointer = C.sp_cursor(db.Pointer, C.sporder(order), unsafe.Pointer(nil), C.size_t(0))
-	} else {
-		cur.Pointer = C.sp_cursor(db.Pointer, C.sporder(order), unsafe.Pointer(&key[0]), C.size_t(len(key)))
-	}
-	if nil == cur.Pointer {
-		return nil, db.Error()
-	}
-	return cur, nil
-}
-
-// Delete deletes the key from the database.
-func (db *Database) Delete(key []byte) error {
-	if 0 != C.sp_delete(db.Pointer, unsafe.Pointer(&key[0]), C.size_t(len(key))) {
-		return db.Error()
-	}
-	return nil
-}
 
 // Has returns true if the database has a value for the key.
 func (db *Database) Has(key []byte) (bool, error) {
@@ -118,6 +155,17 @@ func (db *Database) Has(key []byte) (bool, error) {
 		return true, nil
 	}
 	return false, fmt.Errorf("ERROR: unexpected return value from sp_get: %v", e)
+}
+
+// Rollback discards the changes of a multi-statement
+// transaction. All modifications made during the transaction are not written to 
+// the log file.
+func (db *Database) Rollback() error {
+	e := C.sp_rollback(db.Pointer)
+	if 0!=e {
+		return db.Error()
+	}
+	return nil
 }
 
 // Set sets the value of the key.
--- Gophia_test.go
+++ Gophia_test.go
@@ -1,6 +1,7 @@
 package gophia
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -14,28 +15,28 @@ func TestBasicSanity(t *testing.T) {
 	checkErr(err)
 	defer db.Close()
 
-	err = db.SetStrings("one", "ichi")
+	err = db.SetSS("one", "ichi")
 	checkErr(err)
-	err = db.SetStrings("two", "nichi")
+	err = db.SetSS("two", "nichi")
 	checkErr(err)
-	one, err := db.GetStrings("one")
+	one, err := db.GetSS("one")
 	checkErr(err)
 	if one != "ichi" {
 		t.Fatal("one not saved / restored right")
 	}
-	two, err := db.GetStrings("two")
+	two, err := db.GetSS("two")
 	checkErr(err)
 	if two != "nichi" {
 		t.Fatal("two not saved / restored right")
 	}
-	if !db.MustHasString("one") {
+	if !db.MustHasS("one") {
 		t.Fatal("HasString failed for key in database")
 	}
-	checkErr(db.DeleteString("one"))
-	if db.MustHasString("one") {
+	checkErr(db.DeleteS("one"))
+	if db.MustHasS("one") {
 		t.Fatal("HasString succeeded for key in database")
 	}
-	_, err = db.GetStrings("one")
+	_, err = db.GetSS("one")
 	if err != ErrNotFound {
 		if nil != err {
 			t.Fatalf("Unexpected error retrieving deleted key: %v", err.Error())
@@ -124,4 +125,62 @@ func TestIterator(t *testing.T) {
 		}
 		index++
 	}
+}
+
+func TestTransactions(t *testing.T) {
+	checkErr := func(err error) {
+		if nil!=err {
+			t.Fatal(err)
+		}
+	}
+	db, err := Open(Create, "testdb_trans")
+	defer db.Close()
+	values := [][2]string {
+		[2]string { "one", "ichi"},
+		[2]string{ "two", "nichi" },
+		[2]string{ "three", "san" },
+	}
+	for _, v := range values {
+		checkErr(db.DeleteS(v[0]))
+	}
+	enterValues := func() error {
+		for _,v := range values {
+			checkErr(db.SetSS(v[0],v[1]))
+		}
+		return nil
+	}
+	checkValues := func() (bool, error) {
+		for _,v := range values {
+			val, err := db.GetSS(v[0])
+			if nil!=err {				
+				return false, err
+			}
+			if v[1]!=val {
+				return false, fmt.Errorf("Value of %v didn't match: %v", v[0], val)
+			}
+		}
+		return true, nil
+	}
+
+	checkErr(db.Begin())
+	// Although this should work, it doesn't appear to do so - we don't get the expected error
+	// err = db.Begin()
+	// if err!=ErrTransactionInProgress {
+	// 	t.Fatalf("Error: Transaction in progress but Begin succeeded: err = %v", err)
+	// }
+	checkErr(enterValues())
+	checkErr(db.Rollback())
+	f, err := checkValues()
+	if f || ErrNotFound!=err {
+		t.Errorf("Inserted values in place despite transaction rollback")
+	}
+
+	checkErr(db.Begin())
+	checkErr(enterValues())
+	checkErr(db.Commit())
+	f, err = checkValues()
+	if nil!=err || !f {
+		t.Errorf("Values not in place but transaction committed")
+	}
+
 }
